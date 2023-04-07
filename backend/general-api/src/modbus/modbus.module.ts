091d8dbc89c4bf785f725aed5feb87b2fb2d9663
Istvan Ficzus <istvan@jaspercard.com>
2023-04-07 22:44:46 +0300

    Integrating config

--- backend/general-api/src/modbus/modbus.module.ts
+++ backend/general-api/src/modbus/modbus.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ModbusController } from './modbus.controller';
 import { ModbusService } from './modbus.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [ModbusController],
   providers: [ModbusService]
 })
--- backend/general-api/src/modbus/modbus.service.ts
+++ backend/general-api/src/modbus/modbus.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import ModbusRTU from 'modbus-serial';
 
 @Injectable()
 export class ModbusService {
   private client: ModbusRTU;
 
-  constructor() {
+  constructor(private configService: ConfigService) {
+    const cfg = JSON.parse(this.configService.get('MODBUS'));
+    console.log({ cfg });
     // create an empty modbus client
     this.client = new ModbusRTU();
     // open connection to a serial port
-    this.client.connectRTUBuffered('/dev/ttyUSB0', { baudRate: 9600 });
+    this.client.connectRTUBuffered(cfg.port, {
+      baudRate: cfg.baudrate,
+      parity: cfg.parity,
+      dataBits: cfg.bytesize,
+      stopBits: cfg.stopbits,
+    });
     // set timeout, if slave did not reply back
     this.client.setTimeout(500);
   }
 
   getThisShit() {
-    return this.client.readInputRegisters(0, 1);
+    return this.client.readHoldingRegisters(0, 81);
   }
 }
